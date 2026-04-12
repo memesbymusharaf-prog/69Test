0b3845aa9c8ae3d584d10277dff070d5e1811b6e
memesbymusharaf-prog <memesbymusharaf@gmail.com>
2026-04-13 04:29:43 +0530

    Update bot token and username in config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8496567141:AAFEio3yxeXMQEe7v6WRx8g4czrjuT8T9-A")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8560918964:AAEYqIQrteTNqBTlliDvCp_pDjN1pcCynZg")
 ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "7167704900"))
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "Test_69x69_Bot")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Suspecious_Checker_Bot")
+HIT_DETECTION_GROUP_ID = int(os.environ.get("HIT_DETECTION_GROUP_ID", "-1003838614236"))
