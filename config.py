2b0284fc486474f5e2fc6f60103cd8abba8d2ba9
memesbymusharaf-prog <memesbymusharaf@gmail.com>
2026-04-13 04:49:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,3 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8560918964:AAEYqIQrteTNqBTlliDvCp_pDjN1pcCynZg")
 ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "7167704900"))
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "Suspecious_Checker_Bot")
-HIT_DETECTION_GROUP_ID = int(os.environ.get("HIT_DETECTION_GROUP_ID", "-1003800745799"))
