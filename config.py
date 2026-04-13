62c58b77e5ff138b70615ac5c138140777ab312d
memesbymusharaf-prog <memesbymusharaf@gmail.com>
2026-04-13 05:54:55 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8560918964:AAEYqIQrteTNqBTlliDvCp_pDjN1pcCynZg")
+ADMIN_CHAT_ID = int(os.environ.get("ADMIN_CHAT_ID", "7167704900"))
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "Suspecious_Checker_Bot")
